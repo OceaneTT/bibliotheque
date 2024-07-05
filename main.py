7b0d1289da7b0a4911f95a30dd74761a7d765195
Oceane Taleb <taleboceane@gmail.com>
2024-07-05 17:47:13 +0200

    add users

--- main.py
+++ main.py
@@ -1,8 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+import models
+import schemas
+from database import engine, Base, get_db
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
+
+# create table from models
+models.Base.metadata.create_all(engine)
 
 # home page
 @app.get("/home")
 def home():
     return 'Welcome to ESG Data & IA FastAPI Worskshop'
+
+# add new book 
+@app.post("/book")
+def add_book(request: schemas.book, db: Session = Depends(get_db)):
+    
+    new_book = models.book (title = request.title,
+                           author =  request.author,
+                           description = request.description,
+                           published_year = request.published_year,
+                           publisher = request.publisher
+                        )
+
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+
+    return new_book
+
+# retrieve a list of all books
+@app.get("/books")
+def get_books(db: Session = Depends(get_db)):
+    return db.query(models.book).all()
+
+
+# add new user 
+@app.post("/users")
+def add_user(request: schemas.User, db: Session = Depends(get_db)):
+    
+    new_user = models.User (name = request.name,
+                           birthday = request.birthday,
+                           gender = request.gender,
+                           email = request.email
+                        )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+# retrieve a list of all books
+@app.get("/users")
+def get_user(db: Session = Depends(get_db)):
+    return db.query(models.User).all()
